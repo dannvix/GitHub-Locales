b98e94e92d1088eb5146772b04a9d3e6e107bd1a
Shao-Chung Chen <dannvix@gmail.com>
2013-08-15 12:07:17 +0800

    merge zh-TW translation refinements from @zhusee2 on pull request #1

--- content-script.js
+++ content-script.js
@@ -4,7 +4,7 @@ var GitHubLocales = function(locale) {
       /* global */
       ".*github.com.*": [
         /* top nav-bar */
-        ["ul.top-nav > li.explore > a", "", "瀏覽"], /* Explorer */
+        ["ul.top-nav > li.explore > a", "", "探索"], /* Explorer */
         ["ul.top-nav > li.features > a", "", "功能"], /* Features */
         ["ul.top-nav > li.enterprise > a", "", "企業用戶"], /* Enterprise */
         ["ul.top-nav > li.blog > a", "", "官方部落格"], /* Blog */
@@ -32,13 +32,13 @@ var GitHubLocales = function(locale) {
 
         /* navigation menubar */
         ["ul.dashboard-tabs > li > a[href='/']", "", "動態消息"], /* News Feed */
-        ["ul.dashboard-tabs > li > a[href*=pulls]", "", "同步請求"], /* Pull Requests */
-        ["ul.dashboard-tabs > li > a[href*=issues]", "", "議案列表"], /* Issues */
+        ["ul.dashboard-tabs > li > a[href*=pulls]", "", "收用請求"], /* Pull Requests */
+        ["ul.dashboard-tabs > li > a[href*=issues]", "", "議題列表"], /* Issues */
         ["ul.dashboard-tabs > li > a[href*=stars]", "", "收藏列表"], /* Stars */
 
         /* repository title bar */
         ["h1.entry-title.public > span.repo-label > span", "", "公開專案"], /* Public */
-        ["h1.entry-title span.fork-flag > span.text", "func", function(el){el.html("由 " + $("h1.entry-title span.fork-flag > span.text > a")[0].outerHTML + " 分支而來")}], /* Forked from {repo_name} */
+        ["h1.entry-title span.fork-flag > span.text", "func", function(el){el.html("分支自 " + $("h1.entry-title span.fork-flag > span.text > a")[0].outerHTML)}], /* Forked from {repo_name} */
         ["ul.pagehead-actions span.octicon-eye-watch", "func", function(el){el.parent().html($("ul.pagehead-actions span.octicon-eye-watch")[0].outerHTML + " 關注")}], /* Watch */
         ["ul.pagehead-actions span.octicon-eye-unwatch", "func", function(el){el.parent().html($("ul.pagehead-actions span.octicon-eye-unwatch")[0].outerHTML + " 取消關注")}], /* Unwatch */
         ["ul.pagehead-actions span.octicon-star + span.text", "", "收藏"], /* Star */
@@ -48,8 +48,8 @@ var GitHubLocales = function(locale) {
       /* dashboard page */
       "^http[s]?:\/\/github.com[\/]+$": [
         /* right sidebar */
-        ["div.dashboard-sidebar h3.box-title:first", "", "您有貢獻的專案"], /* Repositories you contribute to */
-        ["div.dashboard-sidebar h3.box-title:last", "func", function(el){el.html("您擁有的專案 " + $("div.dashboard-sidebar h3.box-title:last span.box-title-count")[0].outerHTML)}], /* Your repositories */
+        ["div.dashboard-sidebar h3.box-title:first", "", "您參與的專案"], /* Repositories you contribute to */
+        ["div.dashboard-sidebar h3.box-title:last", "func", function(el){el.html("您的專案 " + $("div.dashboard-sidebar h3.box-title:last span.box-title-count")[0].outerHTML)}], /* Your repositories */
         ["div.dashboard-sidebar div.box-header a[href*=new]", "func", function(el){el.html($("div.dashboard-sidebar div.box-header a[href*=new] > span")[0].outerHTML + " 新建專案")}], /* New repository */
       ],
       
@@ -70,18 +70,18 @@ var GitHubLocales = function(locale) {
         /* repository meta */
         ["div.repository-meta > span.edit-link > a", "", "編輯"], /* Edit */
         ["div.overall-summary ul.numbers-summary > li.commits > a", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li.commits > a span.num")[0].outerHTML + " 次提交")}], /* {commits_count} Commits */
-        ["div.overall-summary ul.numbers-summary > li > a[href*=branches]", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li > a[href*=branches] span.num")[0].outerHTML + " 個旁支")}], /* {branches_count} Branches */
-        ["div.overall-summary ul.numbers-summary > li > a[href*=releases]", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li > a[href*=releases] span.num")[0].outerHTML + " 次發佈")}], /* {releases_count} Releases */
+        ["div.overall-summary ul.numbers-summary > li > a[href*=branches]", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li > a[href*=branches] span.num")[0].outerHTML + " 個分支")}], /* {branches_count} Branches */
+        ["div.overall-summary ul.numbers-summary > li > a[href*=releases]", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li > a[href*=releases] span.num")[0].outerHTML + " 個發佈")}], /* {releases_count} Releases */
         ["div.overall-summary ul.numbers-summary > li > a[href*=contributors]", "func", function(el){el.html($("div.overall-summary ul.numbers-summary > li > a[href*=contributors] span.num")[0].outerHTML + " 位貢獻者")}], /* {contributors_count} Contributors */
         ["div.repo-container a.repository-lang-stats-graph", "original-title", "顯示程式語言統計"], /* Show language statistics */
 
         /* repository sidebar menu */
         ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title=Code] span.full-word", "", "程式碼"], /* Code */
-        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title=Issues] span.full-word", "" ,"議案"], /* Issues */
-        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Pull Requests'] span.full-word", "", "同步請求"], /* Pull Request */
-        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Pulse'] span.full-word", "", "專案動態"], /* Pulse */
+        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title=Issues] span.full-word", "" ,"議題"], /* Issues */
+        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Pull Requests'] span.full-word", "", "收用請求"], /* Pull Request */
+        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Pulse'] span.full-word", "", "專案脈動"], /* Pulse */
         ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Graphs'] span.full-word", "", "統計圖表"], /* Graphs */
-        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Network'] span.full-word", "","旁支總覽"], /* Network */
+        ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Network'] span.full-word", "","分支網路"], /* Network */
         ["div.repository-sidebar div.repo-nav-contents ul.repo-menu li[original-title='Settings'] span.full-word", "","專案設定"], /* Settings */
         ["div.repository-sidebar div.only-with-full-nav a[href*=zip]", "func", function(el){el.html($("div.repository-sidebar div.only-with-full-nav a[href*=zip] > span")[0].outerHTML + " 打包下載 ZIP 檔")}], /* Download ZIP */
       ],
@@ -89,14 +89,14 @@ var GitHubLocales = function(locale) {
       /* repository issues, e.g. https://github.com/dannvix/GitHub-Locales/issues */
       "http[s]?:\/\/github.com\/[^\/]+\/[^\/]+\/issues": [
         /* tab navigation */
-        ["div#issues_next > div.tabnav a[href$=issues]", "", "議案一覽"], /* Browse Issues */
+        ["div#issues_next > div.tabnav a[href$=issues]", "", "議題一覽"], /* Browse Issues */
         ["div#issues_next > div.tabnav a[href$=milestones]", "", "里程碑"], /* Milestones */
-        ["div#issues_next div.tabnav a[href*=new]", "", "提新議案"], /* New Issue */
+        ["div#issues_next div.tabnav a[href*=new]", "", "新建議題"], /* New Issue */
 
         /* left sidebar */
-        ["div#issues_next ul.filter-list a[href*=issues]:first", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*=issues]:first span.count")[0].outerHTML + " 所有議案")}], /* Everyone's Issues */
-        ["div#issues_next ul.filter-list a[href*='issues/milestones'][href*=open]", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*='issues/milestones'][href*=open] span.count")[0].outerHTML + " 進行中里程碑")}], /* Open Issues */
-        ["div#issues_next ul.filter-list a[href*='issues/milestones'][href*=closed]", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*='issues/milestones'][href*=closed] span.count")[0].outerHTML + " 已結案里程碑")}], /* Closed Issues */
+        ["div#issues_next ul.filter-list a[href*=issues]:first", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*=issues]:first span.count")[0].outerHTML + " 所有議題")}], /* Everyone's Issues */
+        ["div#issues_next ul.filter-list a[href*='issues/milestones'][href*=open]", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*='issues/milestones'][href*=open] span.count")[0].outerHTML + " 進行中的里程碑")}], /* Open Issues */
+        ["div#issues_next ul.filter-list a[href*='issues/milestones'][href*=closed]", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*='issues/milestones'][href*=closed] span.count")[0].outerHTML + " 已達成的里程碑")}], /* Closed Issues */
         ["div#issues_next ul.filter-list a[href*=assigned]:first", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*=assigned]:first span.count")[0].outerHTML + " 指派給您的")}], /* Assigned to you */
         ["div#issues_next ul.filter-list a[href*=created_by]:first", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*=created_by]:first span.count")[0].outerHTML + " 您所建立的")}], /* Created by you */
         ["div#issues_next ul.filter-list a[href*=mentioned]:first", "func", function(el){el.html($("div#issues_next ul.filter-list a[href*=mentioned]:first span.count")[0].outerHTML + " 提及您的")}], /* Mentioned you */
